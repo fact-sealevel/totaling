d02c867d276b4bbe97477c22c48ab991d5fdb832
Emma Marshall <emarshall@rhg.com>
2025-11-17 17:08:10 -0700

    formatting nits

--- src/totaling/cli.py
+++ src/totaling/cli.py
@@ -29,9 +29,16 @@ from totaling.core import WorkflowTotaler
     required=True,
     help="Path to write totaled projections netcdf file.",
 )
-def main(name, item, scale, output_path):
+def main(name, 
+         item, 
+         scale, 
+         output_path):
+    
     click.echo("Hello from FACTS totaling!")
+
+    #Make list of input paths
     paths_list = list(item)
+
     # Create totaler obj
     totaler = WorkflowTotaler(
         name=name,
